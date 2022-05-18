a1e552db4b3bc0035864e33af59d12ac49890518
Fernando Oliveira Limões <nandolimoes@hotmail.com>
2022-05-18 07:51:23 -0300

    Criado a Estrutura de pastas e arquivos bases a serem utilizados

---
+++ 1. Introdução ao mundo do desenvolvimento/3. Como é a carreira de desenvolvimento/index.js
@@ -0,0 +1,33 @@
+//1) O que é programação - Isidro Masseto
+
+// Objetivos da programação:
+
+// Desenvolver software para outras pessoas -> Toda empresa é digital ou ainda não percebeu que é digital
+// Um programador identifica a necessidade do usuário ou cliente e transforma a necessidade em uma aplicação
+
+// Quais ferramentas utilizar:
+
+// Linguagem de programação
+// Bancos de dados
+// Ferramentas de comunicação na WWW
+// Gerar identidade visual
+// Interface
+// Front-End
+
+//Pode ser um especialista de cada área:
+
+//Front-end
+//Back-end
+//Banco de dados
+
+//Entregar uma ferramenta, software, aplicativo, sistema, uma página com valor para alguém.
+//SOFTWARE SÓ É SOFTWARE SE AJUDA ALGUEM!
+//Desenvolver software é resolver problemas, utilizando programação como ferramenta e utilizando várias linguagens
+
+
+
+
+
+
+
+
